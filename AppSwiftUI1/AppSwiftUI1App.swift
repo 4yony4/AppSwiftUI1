524f061fb2d4575349a34ac67bb64ef22b9f5d9d
Jonathan Z. Bar-Magen Numhauser <jbarmagen@ucjc.edu>
2021-04-14 17:15:10 +0200

    Initial Commit

---
+++ AppSwiftUI1/AppSwiftUI1App.swift
@@ -0,0 +1,17 @@
+//
+//  AppSwiftUI1App.swift
+//  AppSwiftUI1
+//
+//  Created by Yony Bar-Magen Numhauser on 14/4/21.
+//
+
+import SwiftUI
+
+@main
+struct AppSwiftUI1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppSwiftUI1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppSwiftUI1
+//
+//  Created by Yony Bar-Magen Numhauser on 14/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
